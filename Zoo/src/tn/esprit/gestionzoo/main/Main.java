901624e6cde37cda1a200d5f6cf17ed094063139
Rabeb Bougatef <139494390+Rabeb21@users.noreply.github.com>
2024-10-16 17:01:18 +0200

    Update Main.java

--- Zoo/src/tn/esprit/gestionzoo/main/Main.java
+++ Zoo/src/tn/esprit/gestionzoo/main/Main.java
@@ -6,10 +6,10 @@ import tn.esprit.gestionzoo.entities.Zoo;
 
 public class Main {
     public static void main(String[] args) {
-        Dolphin d1 = new Dolphin("whale","whaly",20 , true , "sea",2.0f);
+        Dolphin d1 = new Dolphin("whale", "whaly", 10, true, "sea",1.5f);
 
 
-        Penguin p1 = new Penguin("penguin","peng", 23 ,  true, "snow", 0.1f);
+        Penguin p1 = new Penguin("penguin", "peng", 7,  true, "snow", 0.5f);
         System.out.println(d1);
         System.out.println(p1);
         d1.swim();
